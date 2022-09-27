620729258a4709b8b4346c59d57d849a56c2464b
Ricky <ricardosergiodf@gmail.com>
2022-09-27 20:14:15 -0300

    Ex2 area e perimetro retangulo

---
+++ fundamentos/bloco-4-introducao-a-javascript/exercicios/ex2.js
@@ -0,0 +1,7 @@
+const base = 5;
+const heigth = 8;
+const area = base * heigth;
+console.log(area);
+
+let perimeter = (base * 2) + (heigth * 2);
+console.log("A soma de todos os lados =", perimeter);
