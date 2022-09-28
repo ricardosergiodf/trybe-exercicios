83ad2efa9badbb04506c3cd7d1fcace719acd0d6
Ricky <ricardosergiodf@gmail.com>
2022-09-27 21:09:28 -0300

    Ajustes

--- fundamentos/bloco-4-introducao-a-javascript/exercicios/ex1.js
+++
@@ -1,6 +0,0 @@
-const myName = "Ricardo";
-const birthCity = "Fortaleza";
-let birthYear = 1999;
-birthYear = 2030;
-birthCity = "Teste" // birthCity não pode ser alterada pois é uma constante.
-console.log(myName, birthCity, birthYear);
