8a4b1e6db044f9c3e0b4ecdaf09553759fc95956
Ricky <ricardosergiodf@gmail.com>
2022-09-27 21:35:39 -0300

    Ex3 feito

---
+++ fundamentos/bloco-4-introducao-a-javascript/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex3.js
@@ -0,0 +1,13 @@
+const a = 10;
+const b = 15;
+const c = 20;
+
+if (a > b && a > c){
+    console.log("O maior é:", a);
+}
+if (b > c && b > a){
+    console.log("O maior é:", b);
+}
+else {
+    console.log("O maior é:", c);
+}
