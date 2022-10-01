8bddfcfb48f30eabfb11a35ac1bb1b302c19d936
ricardosergiodf <ricardosergiodf@gmail.com>
2022-10-01 02:45:08 -0300

    Ex-2 feito

---
+++ fundamentos/bloco-4-introducao-a-javascript/secao-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/funções/ex-2.js
@@ -0,0 +1,10 @@
+function maiorValor (array) {
+    let maior = array[0];
+    for (index = 0; index < array.length; index += 1) {
+        if (array[index] > maior) {
+            maior = index;
+        }
+    }
+    return maior;
+}
+console.log(maiorValor([2, 3, 6, 7, 10, 1]));
