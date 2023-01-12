6268a7c4ae1dd38f442ba36a82231b884ff9adff
ricardosergiodf <ricardosergiodf@gmail.com>
2023-01-12 04:47:44 -0300

    Ex5 Desafio pratico

--- fundamentos/secao-8-js-hofs/dia-3-filter-e-reduce/desafio-pratico.js
+++ fundamentos/secao-8-js-hofs/dia-3-filter-e-reduce/desafio-pratico.js
@@ -101,4 +101,19 @@ const countA = () => {
     }, 0);
 }
 
-console.log(countA());
+//console.log(countA());
+
+// O index 0 do array `students` equivale ao index 0 do array `grades`
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+const studentAverage = () => {
+    return nameAndAverage = students.map((student, index) => (
+        {
+            name: student,
+            average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+        }));
+}
+
+console.log(studentAverage(students, grades));
