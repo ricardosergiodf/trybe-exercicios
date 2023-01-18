fe208455b61e098d3c5d79870ff8b3a6e03369b2
ricardosergiodf <ricardosergiodf@gmail.com>
2023-01-17 23:36:35 -0300

    Validador de campos funcionando

---
+++ front-end/secao-1-introducao-a-frontend-e-js-assincrono/dia-1-ambiente-de-desenvolvimento/validador-de-campos/src/main.js
@@ -0,0 +1,16 @@
+import validator from 'validator';
+
+const campoTexto = document.querySelector('#value');
+const opcao = document.querySelector('#option');
+const botao = document.querySelector('#button');
+const resposta = document.querySelector('#answer');
+
+botao.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const campos = {
+        cpf: validator.isTaxID(campoTexto.value, 'pt-BR'),
+        email: validator.isEmail(campoTexto.value),
+    };
+    resposta.innerHTML = `A validação retornou ${campos[opcao.value]}`;
+});
