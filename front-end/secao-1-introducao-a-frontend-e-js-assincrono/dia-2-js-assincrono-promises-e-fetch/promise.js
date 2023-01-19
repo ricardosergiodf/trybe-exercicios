16fad2d4bc8bb4cffa1615461590d78617eab410
ricardosergiodf <ricardosergiodf@gmail.com>
2023-01-19 18:32:42 -0300

    Exemplo de promises RESOLVIDA e REJEITADA com .THEN e .CATCH

---
+++ front-end/secao-1-introducao-a-frontend-e-js-assincrono/dia-2-js-assincrono-promises-e-fetch/promise.js
@@ -0,0 +1,28 @@
+// função que gera um número aleatório
+const generateRandomNumber = () => Math.round(Math.random() * 10);
+
+// promises Resolvida e Rejeitada com seus respectivos retornos
+const randomPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const randomNumber = generateRandomNumber();
+      if(randomNumber % 2 === 0) {
+        resolve(randomNumber);
+        // Promise retornando número aleatório caso for PAR
+      } else {
+        reject(new Error (`Número gerado rejeitado: ${randomNumber}. Somente números pares.`));
+        // Promise retornando ERRO caso for IMPAR
+      }
+    }, 1000);
+  });
+
+
+randomPromise()
+  .then((response) => {
+    console.log(`Promise resolvida: o número gerado foi ${response}`);
+    // .THEN Retorna a promise Resolvida com o número gerado
+  })
+  .catch((error) => {
+    console.log(`Promise rejeitada: ${error.message}`);
+    // .CATCH Captura a promise Rejeitada e retorna o erro no console.log
+  });
