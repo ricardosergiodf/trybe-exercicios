c4793728c8728567ff3ba5f84beb43d146506977
ricardosergiodf <ricardosergiodf@gmail.com>
2023-01-24 03:49:53 -0300

    Agora funciona

--- front-end/secao-1-introducao-a-frontend-e-js-assincrono/dia-4-async-await-e-testes-assincronos/consulta-de-cep/main.js
+++ front-end/secao-1-introducao-a-frontend-e-js-assincrono/dia-4-async-await-e-testes-assincronos/consulta-de-cep/main.js
@@ -8,14 +8,13 @@ async function clicou() {
     const cep = getCep.value;
 
     try {
-        const result = await fetch('https://viacep.com.br/ws/${cep}/json/');
+        const result = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const data = await result.json();
 
         resposta.innerHTML = JSON.stringify(data)
         console.log(data);
         return data;
     } catch (error) {
-        resposta.innerHTML = error.message;
         return error.message;
     }
 }
