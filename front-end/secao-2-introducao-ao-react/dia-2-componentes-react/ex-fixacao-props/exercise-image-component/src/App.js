a376ff7f08d6220633374555088f6dcef70abff9
ricardosergiodf <ricardosergiodf@gmail.com>
2023-03-08 02:58:39 -0300

    Questão 2

--- front-end/secao-2-introducao-ao-react/dia-2-componentes-react/ex-fixacao-props/exercise-image-component/src/App.js
+++ front-end/secao-2-introducao-ao-react/dia-2-componentes-react/ex-fixacao-props/exercise-image-component/src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Image from './Image';
+import staringCat from './staringCat.jpg'
 
 function App() {
+  // const URL = 'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg';   // Também pode ser usada a URL da imagem
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      { // <Image source={ URL } alternativeText={ 'Cute cat staring' } />
+      }
+      <Image source={ staringCat } alternativeText={ 'Cute cat staring' } />
+    </main>
   );
 }
 
