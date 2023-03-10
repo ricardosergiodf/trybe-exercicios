34392ccc3eb3a8b32972e11385032b7c87ff3ffa
ricardosergiodf <ricardosergiodf@gmail.com>
2023-03-09 23:56:39 -0300

    Implementando o numero de cliques em cada botão

--- front-end/secao-3-componentes-estado-eventos-forms-react/dia-1-componentes-com-estad-e-eventos/ex-fixação/fancy-buttons/src/App.js
+++ front-end/secao-3-componentes-estado-eventos-forms-react/dia-1-componentes-com-estad-e-eventos/ex-fixação/fancy-buttons/src/App.js
@@ -4,28 +4,42 @@ import React from 'react';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      numeroDeCliques1: 0,
+      numeroDeCliques2: 0,
+      numeroDeCliques3: 0,
+    };
+
     this.handleButtonOne = this.handleButtonOne.bind(this);
     this.handleButtonTwo = this.handleButtonTwo.bind(this);
     this.handleButtonThree = this.handleButtonThree.bind(this);
   }
 
   handleButtonOne() {
-    console.log('"this" do botão 1 :', this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques1: estadoAnterior.numeroDeCliques1 + 1,
+    }));
   }
 
   handleButtonTwo() {
-    console.log('"this" do botão 2 :', this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques2: estadoAnterior.numeroDeCliques2 + 1,
+    }));
   }
 
   handleButtonThree() {
-    console.log('"this" do botão 3 :', this);
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques3: estadoAnterior.numeroDeCliques3 + 1,
+    }));
   }
   render() {
+    const { numeroDeCliques1, numeroDeCliques2, numeroDeCliques3 } = this.state;
+
     return (
       <div>
-        <button onClick={ this.handleButtonOne }>Botão 1</button>
-        <button onClick={ this.handleButtonTwo }>Botão 2</button>
-        <button onClick={ this.handleButtonThree }>Botão 3</button>
+        <button onClick={ this.handleButtonOne }>{ numeroDeCliques1 }</button>
+        <button onClick={ this.handleButtonTwo }>{ numeroDeCliques2 }</button>
+        <button onClick={ this.handleButtonThree }>{ numeroDeCliques3 }</button>
       </div>
     );
   }
